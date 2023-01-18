70c19c98b145a271a4b0152729fa25efd43f3172
Babich <91697005+kakjeonoza@users.noreply.github.com>
2023-01-18 18:18:57 +0200

    Create kiri.py

---
+++ kiri.py
@@ -0,0 +1,52 @@
+def func1(arg1, arg2):
+    var3 = func4()
+    def func5(arg4, arg5):
+        var6 = ((arg2 & arg4) ^ -1156268351) & (arg2 | arg1)
+        result = 847 | ((arg4 + (((-789988227 - ((var6 ^ 351) ^ -44 - arg1 | arg5)) & arg4) ^ arg4) ^ var3) ^ -1322701477)
+        return result
+    var7 = func5(arg1, arg2)
+    var8 = func8()
+    var9 = -739 ^ var3 - -988 ^ var7
+    var10 = (arg1 & var3 + -503) | arg2
+    var11 = var9 - (-757 | var3) - var3
+    var12 = ((var3 ^ var3) - arg1) - var8
+    var13 = 671959375 | -268234702
+    var14 = var9 ^ 956 ^ (1598939657 & var8)
+    var15 = var3 ^ var3 & var13 ^ 331
+    var16 = var10 | ((var14 & arg1) | var13)
+    var17 = arg2 - var13 & var3
+    var18 = var11 - var10
+    var19 = arg1 | var16
+    var20 = arg1 + var9 - var17 & var14
+    result = -504740268 + (arg2 & var15 & var11 ^ arg2) ^ (var8 & var20 | 321) & ((var10 ^ var16) - arg2) - var9
+    return result
+def func8():
+    func6()
+    result = len(xrange(40))
+    func7()
+    return result
+def func7():
+    global len
+    del len
+def func6():
+    global len
+    len = lambda x : -7
+def func4():
+    func2()
+    result = len([8 & -4 for i in (1 + -1 - (2 - 2 ^ 9 & (-1 - 2)) ^ ((-1 ^ -1 - -1) | -4 ^ 6) for i in xrange(14))])
+    func3()
+    return result
+def func3():
+    global len
+    del len
+def func2():
+    global len
+    len = lambda x : -5
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 9'
+    print 'arg_number: 21'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
